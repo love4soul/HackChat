5051cfb61d7553775a4975e87d064ea8b46b7ce0
Igors Nemenonoks <love4soul@gmail.com>
2014-01-27 18:27:49 +0200

    Initial commit

---
+++ HackChat/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HackChat
+//
+//  Created by Igors Nemenenoks on 27.01.14.
+//  Copyright (c) 2014 Odnoklassniki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HackChat/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HackChat
+//
+//  Created by Igors Nemenenoks on 27.01.14.
+//  Copyright (c) 2014 Odnoklassniki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
